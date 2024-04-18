d63a4342b09938cf26fc6bcbd31de1e1f148c2ad
Luis Cortorreal <cort0038@algonquinlive.com>
2024-04-18 00:18:58 -0400

    render data from fetch

--- app/crap/page.js
+++ app/crap/page.js
@@ -7,49 +7,71 @@ export default async function Crap({params, searchParams}) {
 	let token = session?.value
 	let keyword = searchParams.keyword
 
-	const response = await fetch(`${process.env.ROOT_URL}/api/crap?keyword=${keyword}&token=${token}`, {
-		method: "GET",
-		headers: {
-			"Accept": "application/json"
-		}
-	})
+	try {
+		const response = await fetch(`${process.env.ROOT_URL}/api/crap?keyword=${keyword}&token=${token}`, {
+			method: "GET",
+			headers: {
+				"Accept": "application/json"
+			}
+		})
 
-	if (response.status === 200) {
-		let data = await response.json()
-		if (data.length === 0) {
+		if (response.status === 200) {
+			let data = await response.json()
+
 			return (
 				<>
 					<SearchBar />
 
-					<div className="flex flex-col items-center justify-center pt-16">
-						<div className="flex gap-1">
-							<p className="font-bold text-xl text-red-600 text-center">No items found for</p>
-							<p className="text-xl text-red-600 text-center font-bold italic">&quot;{keyword}&quot;</p>
-						</div>
-						<p className="text-center py-3">If you think this is a mistake, please, try again later.</p>
+					{data && data.length === 0 && (
+						<div className="flex flex-col items-center justify-center pt-16">
+							<div className="flex gap-1">
+								<p className="font-bold text-xl text-red-600 text-center">No items found for</p>
+								<p className="text-xl text-red-600 text-center font-bold italic">&quot;{keyword}&quot;</p>
+							</div>
+							<p className="text-center py-3">If you think this is a mistake, please try again later.</p>
 
-						<Image src="/404-page-not-found.svg" alt="404 Error Image" width={500} height={500} className="py-4" />
-					</div>
+							<Image
+								src="/404-page-not-found.svg"
+								alt="404 Error Image"
+								width={500}
+								height={500}
+								className="py-4"
+							/>
+						</div>
+					)}
+
+					{data && data.length > 0 && (
+						<div>
+							{data.map((item, index) => (
+								<div key={index}>
+									<h2>{item.title}</h2>
+									<p>{item.description}</p>
+									{item.images.map((imageUrl, imageIndex) => (
+										<div key={imageIndex}>
+											<Image src={imageUrl} alt={item.description} width={500} height={500} />
+										</div>
+									))}
+									<p>{item.status}</p>
+									<p>{item.owner.name}</p>
+								</div>
+							))}
+						</div>
+					)}
 				</>
 			)
 		} else {
 			return (
-				<div>
-					<h1>Crap</h1>
-					<p>{JSON.stringify(data)}</p>
-				</div>
+				<>
+					<SearchBar />
+					<div className="flex flex-col justify-center items-center py-32 text-xl">
+						<h1 className="font-bold italic text-red-800">Something went wrong</h1>
+						<p>Try again later.</p>
+					</div>
+				</>
 			)
 		}
-	} else if (response.status === 401) {
-		console.log(JSON.stringify({ERROR: "Unauthorized. Please, log in."}))
-		return (
-			<div>
-				<h1>Unauthorized</h1>
-				<p>Please, log in.</p>
-			</div>
-		)
-	} else {
-		console.error(JSON.stringify({ERROR: "Something went wrong"}))
+	} catch (error) {
+		console.error(error)
 		return (
 			<div>
 				<h1>Something went wrong</h1>
