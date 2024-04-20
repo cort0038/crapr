83cd100ea1dedf951df0d8fbadc0a618da3c42cd
Luis Cortorreal <cort0038@algonquinlive.com>
2024-04-19 20:11:30 -0400

    added show interest functionalities

---
+++ app/api/interested/route.js
@@ -0,0 +1,41 @@
+export async function GET(request) {
+	try {
+		const token = request.nextUrl.searchParams.get("token")
+		let url = `${process.env.API_URL}/api/crap/interested`
+
+		const response = await fetch(url, {
+			method: "GET",
+			headers: {
+				"Accept": "application/json",
+				"Authorization": `Bearer ${token}`
+			},
+			next: {revalidate: 60}
+		})
+
+		if (response.status === 200) {
+			let data = await response.json()
+			return new Response(JSON.stringify(data), {
+				status: 200,
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+		} else if (response.status === 401) {
+			return new Response(JSON.stringify({error: "Unauthorized. Please, log in."}), {
+				status: 401,
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+		} else {
+			throw new Error("Request failed with status " + response.status)
+		}
+	} catch (error) {
+		return new Response(JSON.stringify({error: "Something went wrong"}), {
+			status: 500,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+	}
+}
--- app/crap/[id]/page.js
+++ app/crap/[id]/page.js
@@ -1,15 +1,42 @@
 import {getSession} from "@/app/actions"
 import Image from "next/image"
 import {decodeToken} from "@/app/actions"
+import {revalidatePath} from "next/cache"
 
 export default async function crapId(params) {
+	async function showInterest() {
+		"use server"
+		const url2 = `${process.env.API_URL}/api/crap/${params.params.id}/interested`
+
+		const postResponse = await fetch(url2, {
+			method: "PATCH",
+			headers: {
+				Accept: "application/json",
+				Authorization: `Bearer ${token}`
+			},
+			body: JSON.stringify({
+				status: "Interested"
+			}),
+			next: {revalidate: 60}
+		})
+
+		if (postResponse.ok) {
+			const postData = await postResponse.json()
+			const crapId = postData._id
+
+			if (postResponse.status === 200) {
+				revalidatePath(`/crap/${crapId}`)
+			}
+		}
+	}
+
 	try {
 		const session = await getSession()
 		const token = session?.value
 		const userId = await decodeToken()
 
 		const url = `${process.env.API_URL}/api/crap/${params.params.id}`
-		const response = await fetch(url, {
+		const getResponse = await fetch(url, {
 			method: "GET",
 			headers: {
 				Accept: "application/json",
@@ -17,12 +44,12 @@ export default async function crapId(params) {
 			}
 		})
 
-		if (response.ok) {
-			const data = await response.json()
+		if (getResponse.ok) {
+			const data = await getResponse.json()
 
 			return (
 				<>
-					{data && data.data.status === "Interested" && (
+					{data && data.data.status === "Interested" && data.data.owner._id === userId && (
 						<>
 							<div className="flex items-center justify-center pt-16 gap-12">
 								<a className="flex flex-col border-2 border-black rounded-md gap-3">
@@ -44,7 +71,7 @@ export default async function crapId(params) {
 									</div>
 									{userId === data.data.owner._id && (
 										<div className="flex justify-center py-2">
-											<form action={await deleteCrap(params.params.id)}>
+											<form>
 												<button className="bg-red-600 text-white rounded-md px-3 py-1">Delete</button>
 											</form>
 										</div>
@@ -75,6 +102,53 @@ export default async function crapId(params) {
 						</>
 					)}
 
+					{data && data.data.status === "Interested" && data.data.buyer._id == userId && (
+						<>
+							<div className="flex items-center justify-center pt-16 gap-12">
+								<a className="flex flex-col border-2 border-black rounded-md gap-3">
+									<Image src={data.data.images[0]} alt={data.data.description} width={500} height={500} />
+
+									<div className="p-2">
+										<div className="flex justify-between pb-4 items-center">
+											<p className="font-bold">{data.data.title}</p>
+											<p
+												className={`text-normal ${
+													data.data.status === "Interested"
+														? "bg-blue-300"
+														: data.data.status === "Flushed"
+														? "bg-red-300"
+														: data.data.status === "Scheduled"
+														? "bg-orange-300"
+														: data.data.status === "Agreed"
+														? "bg-yellow-300"
+														: "bg-green-300"
+												} w-fit rounded-xl px-2`}>
+												{data.data.status}
+											</p>
+										</div>
+										<div className="border-b-2 pt-2">
+											<p className="font-bold">Description</p>
+											<p className="italic">{data.data.description}</p>
+										</div>
+										<div className="flex flex-col pt-2">
+											<p className="font-bold">Buyer:</p>
+											<p>{data.data.buyer.name}</p>
+										</div>
+									</div>
+									{userId === data.data.owner._id && (
+										<div className="flex justify-center py-2">
+											<form>
+												<button className="bg-red-600 text-white rounded-md px-3 py-1">Delete</button>
+											</form>
+										</div>
+									)}
+								</a>
+
+								<p className="text-xl font-bold">Waiting for details from seller...</p>
+							</div>
+						</>
+					)}
+
 					{data && data.data.status === "Available" && (
 						<>
 							<div className="flex items-center justify-center pt-16 gap-12">
@@ -84,7 +158,20 @@ export default async function crapId(params) {
 									<div className="p-2">
 										<div className="flex justify-between pb-4 items-center">
 											<p className="font-bold">{data.data.title}</p>
-											<p className="text-normal bg-green-300 w-fit rounded-xl px-2">{data.data.status}</p>
+											<p
+												className={`text-normal ${
+													data.data.status === "Interested"
+														? "bg-blue-300"
+														: data.data.status === "Flushed"
+														? "bg-red-300"
+														: data.data.status === "Scheduled"
+														? "bg-orange-300"
+														: data.data.status === "Agreed"
+														? "bg-yellow-300"
+														: "bg-green-300"
+												} w-fit rounded-xl px-2`}>
+												{data.data.status}
+											</p>
 										</div>
 										<div className="border-b-2 pt-2">
 											<p className="font-bold">Description</p>
@@ -103,7 +190,7 @@ export default async function crapId(params) {
 									)}
 									{userId !== data.data.owner._id && data.data.status === "Available" && (
 										<div className="flex justify-center py-2">
-											<form>
+											<form action={showInterest}>
 												<button className="bg-blue-600 text-white rounded-md px-3 py-1">I am interested</button>
 											</form>
 										</div>
